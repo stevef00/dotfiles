0722a9c00806c204fb567231acbe45af2378000c
Steve Feehan <stevef00@pobox.com>
2024-07-24 12:17:54 -0400

    bashrc.d/vim.sh: fetch junegunn/vim-plug if it doesn't exist

--- .bashrc.d/vi.sh
+++ .bashrc.d/vi.sh
@@ -1,6 +1,21 @@
 if type nvim >/dev/null 2>&1; then
   alias vi=nvim
   export EDITOR=nvim
+
+  if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]; then
+    echo -n "Fetching 'junegunn/vim-plug' ... "
+    if curl -sfLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim; then
+      echo "done"
+      echo
+      echo "Start nvim and run:"
+      echo " :PlugInstall"
+      echo
+    else
+      echo "failed."
+    fi
+  fi
+
 elif type vim >/dev/null 2>&1; then
   alias vi=vim
   export EDITOR=vim
