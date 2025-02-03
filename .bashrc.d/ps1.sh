3c609d953ff0c8ea803d81d5f7e6af4ffaca9259
Steve Feehan <stevef00@pobox.com>
2025-02-03 11:48:36 -0500

    direnv: start work on direnv support

--- .bashrc.d/ps1.sh
+++ .bashrc.d/ps1.sh
@@ -1,3 +1,10 @@
+show_virtual_env() {
+  if [[ -n "$VIRTUAL_ENV" && -n "$DIRENV_DIR" ]]; then
+    echo "($(basename $VIRTUAL_ENV)) "
+  fi
+}
+export -f show_virtual_env
+
 function ps1_parse_git_branch {
       local branch="$(git branch --show-current 2>/dev/null || : )"
       if [ -n "$branch" ]; then
@@ -23,8 +30,10 @@ case "$HOSTNAME" in
 esac
 _prompt_color_git="97"   # light purple
 _prompt_color_root="240" # light grey
+_prompt_color_virtual_env="100" # yellow
 if type git >/dev/null 2>&1; then
   PS1="\[$(_fg_color ${_prompt_color_git})\]\$(ps1_parse_git_branch)\[$(_fg_color ${_prompt_color_default})\]\u@\h \w \[$(_fg_color ${_prompt_color_root})\]\$${_prompt_reset} "
+  PS1="\[$(_fg_color ${_prompt_color_virtual_env})\]\$(show_virtual_env)"$PS1
 else
   PS1="[\u@\h \w] \$ "
 fi
